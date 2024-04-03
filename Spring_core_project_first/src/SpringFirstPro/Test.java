86278f1c15a69f8616fc47d6a409483392105d1e
Rajesh Gajraj <121161516+rajeshgajraj32@users.noreply.github.com>
2024-04-03 15:49:55 +0530

    update in program

--- Spring_core_project_first/src/SpringFirstPro/Test.java
+++ Spring_core_project_first/src/SpringFirstPro/Test.java
@@ -9,16 +9,29 @@ public class Test {
 
 	public static void main(String[] args) {
 		    Resource resource=new ClassPathResource("FirstProject.xml");  
+		    System.out.println(resource);
 		    BeanFactory factory=new XmlBeanFactory(resource);
+		    System.out.println(factory);
 		    
 		    Ram ram=(Ram) factory.getBean("ram");
 		    ram.add();
 		    ram.remove();
+		    System.out.println(ram);
 		    
+		    Ram ram2=(Ram) factory.getBean("ram");
+		    ram2.add();
+		    ram2.remove(); 
+		    System.out.println(ram2);
 		    
 		    Shyam shyam=(Shyam) factory.getBean("shyam");
 		    shyam.add();
 		    shyam.remove();
+		    System.out.println(shyam);
+		    
+		    Ram ramSecond=(Ram) factory.getBean("ram2");
+		    ramSecond.add();
+		    ramSecond.remove();
+		    System.out.println(ramSecond);
 	}
 
 }
